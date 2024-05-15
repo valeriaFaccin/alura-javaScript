6348ada3fd823cba237df5ca66e4ed74b7edf7c3
Valéria Faccin <valeriafaccin00@gmail.com>
2024-05-14 22:07:32 -0300

    adicionando validação

---
+++ app/verificacao.js
@@ -0,0 +1,48 @@
+function verificaValorValidoChute (chute){
+    const numero =+ chute;
+
+    if(chuteForInvalido(numero)){
+        elementoChute.innerHTML = `
+            <div>Valor inválido</div>
+        `;
+        return;
+    }
+
+    if(numeroValorMaiorMenorRange(numero)){
+        elementoChute.innerHTML = `
+            <div>Valor inválido: o número precisa estar entre ${menorValor} e ${maiorValor}</div>
+        `;
+        return;
+    }
+
+    if(numero === numeroAleatorio){
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroAleatorio}</h3>
+            <button id="jogar-novamente" class="jogar-novamente-btn">Jogar Novamente</button>
+        `;
+    } else if (numero < numeroAleatorio) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>
+        `
+    }
+   
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroValorMaiorMenorRange(numero) {
+    return  numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload();
+    }
+});
